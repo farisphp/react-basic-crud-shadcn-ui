c0a94b2a745a42d9453216c8147cf37191f6d02c
Faris Perwira <faris.perwira@gmail.com>
2023-11-26 22:30:24 +0800

    feat(theme): Implement change theme

--- src/components/shared/layout/index.tsx
+++ src/components/shared/layout/index.tsx
@@ -6,68 +6,56 @@ import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuGroup,
   DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuShortcut,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+
+import { ThemeProvider } from "./theme-provider";
+import { ThemeToggle } from "./theme-toggle";
 
 export const Layout = () => {
   const queryClient = new QueryClient();
   return (
-    <QueryClientProvider client={queryClient}>
-      <main className="container">
-        <section>
-          <div className="flex items-center justify-between my-8">
-            <div>
-              <h2 className="text-2xl font-bold tracking-tight">
-                Welcome back!
-              </h2>
-              <p className="text-muted-foreground">
-                Here&apos;s a list of users!
-              </p>
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+      <QueryClientProvider client={queryClient}>
+        <main className="container">
+          <section>
+            <div className="flex items-center justify-between my-8">
+              <div>
+                <h2 className="text-2xl font-bold tracking-tight">
+                  Welcome back!
+                </h2>
+                <p className="text-muted-foreground">
+                  Here&apos;s a list of users!
+                </p>
+              </div>
+              <div className="flex gap-4 items-center">
+                <ThemeToggle />
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button
+                      variant="ghost"
+                      className="relative h-8 w-8 rounded-full"
+                    >
+                      <Avatar className="h-9 w-9">
+                        <AvatarImage
+                          src="https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/79/79526e939bec9a58c5bdb3dce76c525ca7a1d344_full.jpg"
+                          alt="Faris Perwira"
+                        />
+                        <AvatarFallback>U</AvatarFallback>
+                      </Avatar>
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent className="w-56" align="end" forceMount>
+                    <DropdownMenuItem>Log out</DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </div>
             </div>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button
-                  variant="ghost"
-                  className="relative h-8 w-8 rounded-full"
-                >
-                  <Avatar className="h-9 w-9">
-                    <AvatarImage src="/avatars/03.png" alt="@shadcn" />
-                    <AvatarFallback>SC</AvatarFallback>
-                  </Avatar>
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent className="w-56" align="end" forceMount>
-                <DropdownMenuLabel className="font-normal">
-                  <div className="flex flex-col space-y-1">
-                    <p className="text-sm font-medium leading-none">shadcn</p>
-                    <p className="text-xs leading-none text-muted-foreground">
-                      m@example.com
-                    </p>
-                  </div>
-                </DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuGroup>
-                  <DropdownMenuItem>
-                    Settings
-                    {/* <DropdownMenuShortcut>⌘S</DropdownMenuShortcut> */}
-                  </DropdownMenuItem>
-                </DropdownMenuGroup>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>
-                  Log out
-                  {/* <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut> */}
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </div>
-          <Outlet />
-        </section>
-      </main>
-    </QueryClientProvider>
+            <Outlet />
+          </section>
+        </main>
+      </QueryClientProvider>
+    </ThemeProvider>
   );
 };
